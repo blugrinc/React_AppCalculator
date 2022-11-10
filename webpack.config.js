9eb1ae5e48b9840a8c4fcdfd6f99413dc8adbd94
Luigi Catania <102974286+blugrinc@users.noreply.github.com>
2022-11-10 15:09:12 +0100

    feat(General): absolute import test

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    extensions: ['js', 'ts'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      // ...etc
+    },
+  },
+}
