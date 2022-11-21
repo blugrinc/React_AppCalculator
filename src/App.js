880abb943f15228ad5fc3c9fd768a75c5e16a8f9
Luigi Catania <102974286+blugrinc@users.noreply.github.com>
2022-11-21 16:28:33 +0100

    Update App.js
    Remove uuseless comments

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Button from "./components/keyboard/Button";
 import CalculatorProvider from "./context/CalculatorContext"
 
 //Instanzio un array con tutti i bottoni che voglo nella mia calcolatrice. 
-//Poi facciop il rendering multiplo con map dopo aver concatenato con flat gli elemtneti degli array
+//Poi faccio il rendering multiplo con map dopo aver concatenato con flat gli elemtneti degli array
 
 const btnValues = [  
   [ "7", "8", "9", "/" ],
@@ -14,32 +14,6 @@ const btnValues = [
   [ ".", "0", "C", "-" ],
   ["DEL", "="]
 ]
-
-/*const testA = {
-  a: 1,
-  b: 2
-} */
-
-// testA?.a - dot notation
-// const {a} = testA - destrutturazione
-// const { a: nuovoNomeVariabile } = testA; - cambiare nome all'oggetto
-
-/* const testB = {
-  1: 1,
-  2: 2,
-  3: 3,
-  4: 4,
-  5: 5
-}*/
-
-//test["a"] - object mapping
-/* const lengthObjectPropertiesToArray = Object.keys(testB).length;
-let i = 0;
-while (i < lengthObjectPropertiesToArray) {  
-  console.log(testB[ i ]);
-  i++
-} 
-*/
 
 
 function App() {
